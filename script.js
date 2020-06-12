62827cc0240ff495bf6f9773a63f93dd97b3df17
Matej Boháč <bilboturambar@gmail.com>
2020-06-12 17:30:49 +0000

    drobne opravy

--- script.js
+++ script.js
@@ -1,22 +1,39 @@
-$(() => {
+$(function() {
   startButton();
   endButton();
-})
+});
 
 function endButton() {
-  let gameMenu = $('#game-menu');
-  let gameEnd = $('#game-end');
+  var gameMenu = $('#game-menu');
+  var gameEnd = $('#game-end');
   gameMenu.find('#end-button').click(() => {
     gameMenu.hide();
     gameEnd.show();
   });
+
+  /*gameMenu.find('#end-button').click(function() {
+    gameMenu.hide();
+    gameEnd.show();
+  });*/
 }
 
 function startButton() {
-  let gameMenu = $('#game-menu');
-  let gamePlay = $('#game-play');
+  var gameMenu = $('#game-menu');
+  var gamePlay = $('#game-play');
   gameMenu.find('#play-button').click(() => {
     gameMenu.hide();
     gamePlay.show();
-  })
+  });
+
+  //porovnanie javascript a jquery
+  function info() {
+    var gameMenu = $('#game-menu'); //jquery
+    var gameMenu2 = document.getElementById('game-menu'); //javascript
+    gameMenu.click(() => {
+      //nieco co sa ma vykonat toto je JQUERY
+    });
+    gameMenu2.addEventListener('click', function() {
+      //nieco co sa ma vykonat, toto je cisty javascript
+    });
+  }
 }
