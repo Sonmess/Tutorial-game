fcf654807b83bd682d533c9c05feb34a1caec7d7
Matej Boháč <bilboturambar@gmail.com>
2020-06-12 12:06:06 +0000

    Lesson 2 preparations

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 $(() => {
   startButton();
+  optionsButton();
   endButton();
+  inputName();
+  backButton();
 })
 
 function endButton() {
@@ -19,4 +22,30 @@ function startButton() {
     gameMenu.hide();
     gamePlay.show();
   })
+}
+
+function optionsButton() {
+  let gameMenu = $('#game-menu');
+  let gameOptions = $('#game-options');
+  $('#options-button').click(() => {
+    gameMenu.hide();
+    gameOptions.show();
+  });
+}
+
+function inputName() {
+  let nameInput = $('#options-name');
+  nameInput.change(() => {
+    let playerName = nameInput.val();
+    $('.player-name').text(playerName);
+  })
+}
+
+function backButton() {
+  $('.back-link').click(() => {
+    $('#game-menu').show();
+    $('#game-options').hide();
+    $('#game-play').hide();
+    $("#game-end").hide();
+  });
 }
